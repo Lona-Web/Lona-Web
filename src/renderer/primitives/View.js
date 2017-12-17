2d03e84fd7365350f7c838673a1d695352be2af5
Guillaume Salles <guillaume.salles@me.com>
2017-12-17 13:42:40 -0500

    fix view flex shrink

--- src/renderer/primitives/View.js
+++ src/renderer/primitives/View.js
@@ -30,7 +30,7 @@ export default class View extends React.Component<Props, void> {
           display: 'flex',
           flexDirection: getOrDefault(layer.parameters.flexDirection, 'column'),
           alignItems: getOrDefault(layer.parameters.alignItems, 'stretch'),
-          flexShrink: 0,
+          flexShrink: getOrDefault(layer.parameters.flex, 0),
           alignContent: 'flex-start',
           alignSelf: getOrDefault(layer.parameters.alignSelf, ''),
           justifyContent: getOrDefault(layer.parameters.justifyContent, ''),
